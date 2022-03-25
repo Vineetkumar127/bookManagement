40f109d8b34407c203bd7e9cafec49db07b1eccc
Manish-162403 <98702615+Manish-162403@users.noreply.github.com>
2022-03-25 16:30:16 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,6 @@ const userController = require("../controller/userController")
 
 router.post("/createUser", userController.createUser)
 
-
+router.post("/login",userController.userLogin )
 
 module.exports = router;
